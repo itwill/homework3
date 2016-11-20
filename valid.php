30e08770859931d8a8c728116ba7434f67d45d73
Svetlana Chayka <probnii@mail.ru>
2016-11-20 19:06:37 +0500

    Добавлена функция проверки расширения фотографии

--- valid.php
+++ valid.php
@@ -1,6 +1,27 @@
 <?php
 
-function validText($text){
+function validText($text)
+{
     $text = htmlspecialchars(stripcslashes(trim($text)));
     return $text;
+}
+
+function validPhoto($file)
+{
+    if (isset($file)) {
+        $dir = 'photos';
+        // проверяем загружаемую фотографию
+        $photo_ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+        $extention = ['png','gif','jpg','jpeg'];
+        // проверка соответствия нужным расширениям
+        if ($file === ""){
+            return false;
+        }
+
+        if (!in_array($photo_ext, $extention)) {
+            echo "Неправильный файл. Загрузите картинку";
+            return false;
+        }
+        return $file;
+    }
 }
